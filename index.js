7f634d0da72a03edf9f5a6a133af7a2b49d6e3ee
Michael Uman <Michael_Uman@sigmadesigns.com>
2017-10-04 14:33:28 -0700

    Initial commit of electron-memory-test
    * Electron app to demonstrate limitation of ArrayBuffer allocation.

---
+++ index.js
@@ -0,0 +1,36 @@
+/**
+ *  main entry point for Electron
+ */
+
+const   {app, BrowserWindow, Menu} = require('electron')
+const   path  = require('path')
+const   url   = require('url')
+const   fs    = require('fs');
+
+let     mainWindow = undefined;
+
+console.log("OK " + __dirname);
+
+/** Create the browser window */
+function createWindow() {
+    mainWindow = new BrowserWindow( { width: 800, height: 600 } );
+
+    mainWindow.on('closed', () => {
+        console.log("mainwindow closed!");
+        mainWindow = null;
+    });
+
+    let loadURL = url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true,
+    });
+
+    console.log("loading " + loadURL.toString());
+    mainWindow.loadURL(loadURL);
+}
+
+
+app.on('ready', createWindow);
+
+
